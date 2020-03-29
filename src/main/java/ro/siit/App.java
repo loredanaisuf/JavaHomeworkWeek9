6658b7518453bc972f47aff32cb16a3b91af6883
loredanaisuf <44907592+loredanaisuf@users.noreply.github.com>
2020-03-29 16:58:12 +0300

    last commit

--- src/main/java/ro/siit/App.java
+++ src/main/java/ro/siit/App.java
@@ -17,5 +17,40 @@ public class App
 //           System.out.println("Insert the number: ");
 //
 //       }
+
+        Calculator calculator = new Calculator();
+        String expresion = "10 cm + 1 m - 10 mm + 1 m - 0.9 dm";
+        String[] splitExpresion = expresion.split(" ");
+        for(String s : splitExpresion)
+            System.out.println(s);
+        double number,result=0.0;
+        String formatA;
+        char operand;
+        System.out.println(splitExpresion.length );
+        System.out.println(splitExpresion.length-5);
+        result = Double.parseDouble(splitExpresion[0]);
+        formatA = splitExpresion[1];
+        result = calculator.convertToMeter(result,formatA);
+        //System.out.println("nr= " + result);
+        //System.out.println("Intermediate result is: " + result);
+        System.out.print(result + " ");
+
+        for(int i=2; i<splitExpresion.length-2; i = i+3){
+
+            operand = splitExpresion[i].charAt(0);
+            //System.out.println("op is: " + operand);
+            System.out.print(operand + " ");
+
+            number = Double.parseDouble(splitExpresion[i+1]);
+            formatA = splitExpresion[i+2];
+            number = calculator.convertToMeter(number,formatA);
+            //System.out.println("nr= " + number);
+            System.out.print(number + " = ");
+            result = calculator.calculate(result,operand,number);
+            //System.out.println("Intermediate result is: " + result);
+            System.out.print(result + " ");
+        }
+        System.out.print("\n");
+        System.out.println("Result is: " + result);
     }
 }
--- src/test/java/ro/siit/AppTest.java
+++ src/test/java/ro/siit/AppTest.java
@@ -25,10 +25,138 @@ public class AppTest
          double a = calculator.convertToMeter(10, "cm");
          double b = calculator.convertToMeter(1, "m");
          double result = calculator.calculate(a, '+', b);
-         double c =  calculator.convertToMeter(10,"mm");
-         result = calculator.calculate(result, '-',c);
-         result = calculator.convertFromMeter(result, "mm");
-         assertEquals(calculator.convertFromMeter(a+b-c,"mm"),result,0);
 
+         assertEquals(a+b,result,0);
+
+    }
+
+    @Test
+    public void test2()
+    {
+        Calculator calculator = new Calculator();
+//         Scanner keyboard = new Scanner(System.in);
+//         System.out.println("Insert the number: ");
+//         double nr = keyboard.nextInt();
+        double a = calculator.convertToMeter(10, "cm");
+        double b = calculator.convertToMeter(1, "m");
+        double result = calculator.calculate(a, '+', b);
+        double c =  calculator.convertToMeter(10,"mm");
+        result = calculator.calculate(result, '-',c);
+        assertEquals(a+b-c,result,0);
+    }
+
+    @Test
+    public void test3()
+    {
+        Calculator calculator = new Calculator();
+//         Scanner keyboard = new Scanner(System.in);
+//         System.out.println("Insert the number: ");
+//         double nr = keyboard.nextInt();
+        double a = calculator.convertToMeter(10, "cm");
+        double b = calculator.convertToMeter(1, "m");
+        double result = calculator.calculate(a, '+', b);
+        double c =  calculator.convertToMeter(10,"mm");
+        result = calculator.calculate(result, '-',c);
+        result = calculator.convertFromMeter(result, "mm");
+        assertEquals(calculator.convertFromMeter(a+b-c,"mm"),result,0);
+    }
+
+    @Test
+    public void test4()
+    {
+        Calculator calculator = new Calculator();
+//         Scanner keyboard = new Scanner(System.in);
+//         System.out.println("Insert the number: ");
+//         double nr = keyboard.nextInt();
+        double a = calculator.convertToMeter(10, "cm");
+        double b = calculator.convertToMeter(1, "m");
+        double c =  calculator.convertToMeter(10,"dm");
+        double d =  calculator.convertToMeter(0.2,"km");
+
+        double result = calculator.calculate(a, '+', b);
+        result = calculator.calculate(result, '-',c);
+        result = calculator.calculate(result, '-',d);
+
+        result = calculator.convertFromMeter(result, "km");
+        assertEquals(calculator.convertFromMeter(a+b-c-d,"km"),result,0);
+    }
+
+    @Test
+    public void test5()
+    {
+        Calculator calculator = new Calculator();
+//         Scanner keyboard = new Scanner(System.in);
+//         System.out.println("Insert the number: ");
+//         double nr = keyboard.nextInt();
+        double a = calculator.convertToMeter(10, "cm");
+        double b = calculator.convertToMeter(1, "m");
+        double c =  calculator.convertToMeter(10,"dm");
+        double d =  calculator.convertToMeter(0.2,"km");
+
+        double result = calculator.calculate(a, '+', b);
+        result = calculator.calculate(result, '-',c);
+        result = calculator.calculate(result, '-',d);
+
+        result = calculator.convertFromMeter(result, "km");
+        assertEquals(calculator.convertFromMeter(a+b-c-d,"km"),result,0);
+
+    }
+
+    @Test
+    public void test6()
+    {
+        Calculator calculator = new Calculator();
+        String expresion = "10 cm + 1 m - 10 mm + 1 km - 0.9 dm";
+        double expected = calculator.convertToMeter(10, "cm") + 1 - calculator.convertToMeter(10, "mm") + calculator.convertToMeter(1, "km") - calculator.convertToMeter(0.9, "dm");
+        String[] splitExpresion = expresion.split(" ");
+        for(String s : splitExpresion)
+            System.out.println(s);
+        double number,result=0.0;
+        String formatNumber;
+        char operand;
+        System.out.println(splitExpresion.length );
+        System.out.println(splitExpresion.length-5);
+        result = Double.parseDouble(splitExpresion[0]);
+        formatNumber = splitExpresion[1];
+        result = calculator.convertToMeter(result,formatNumber);
+
+        for(int i=2; i<splitExpresion.length-2; i = i+3){
+            operand = splitExpresion[i].charAt(0);
+            number = Double.parseDouble(splitExpresion[i+1]);
+            formatNumber = splitExpresion[i+2];
+            number = calculator.convertToMeter(number,formatNumber);
+            result = calculator.calculate(result,operand,number);
+        }
+
+        assertEquals(expected,result,0);
+    }
+
+    @Test
+    public void test7()
+    {
+        Calculator calculator = new Calculator();
+        String expresion = "10 cm + 1 m - 10 mm + 1 km - 0.9 dm";
+        double expected = calculator.convertToMeter(10, "cm") + 1 - calculator.convertToMeter(10, "mm") + calculator.convertToMeter(1, "km") - calculator.convertToMeter(0.9, "dm");
+        String[] splitExpresion = expresion.split(" ");
+//        for(String s : splitExpresion)
+//            System.out.println(s);
+        double number,result=0.0;
+        String formatNumber;
+        char operand;
+
+        result = Double.parseDouble(splitExpresion[0]);
+        formatNumber = splitExpresion[1];
+        result = calculator.convertToMeter(result,formatNumber);
+
+        for(int i=2; i<splitExpresion.length-2; i = i+3){
+            operand = splitExpresion[i].charAt(0);
+            number = Double.parseDouble(splitExpresion[i+1]);
+            formatNumber = splitExpresion[i+2];
+            number = calculator.convertToMeter(number,formatNumber);
+            result = calculator.calculate(result,operand,number);
+        }
+
+        result = calculator.convertFromMeter(result, "dm");
+        assertEquals(calculator.convertFromMeter(expected, "dm"),result,0);
     }
 }
